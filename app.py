9f4c2cac44cb12111be4168bc14a137909f6126d
Rohini <mandatirohinib4u@gmail.com>
2025-03-18 17:40:24 +0530

    Initial commit: Flask resume screener app

--- app.py
+++ app.py
@@ -267,5 +267,5 @@ def view_resume(filename):
                           suggestions=suggestions)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT environment variable is not set
+    port = int(os.environ.get('PORT', 10000))  # Default to 5000 if PORT environment variable is not set
     app.run(debug=True, host='0.0.0.0', port=port)
